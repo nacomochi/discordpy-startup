e6112b491f4edc51bd735fa1b83bf0f4885f9f39
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-01-20 07:51:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,30 +14,20 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+@bot.event
+async def on_message(message):
+    if message.content == "/roles":
+        await message.channel.send(message.guild.roles)
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-    
-def get_data(message):
-    command = message.content
-    data_table = {
-        '/members': message.guild.members,
-        '/roles': message.guild.roles,
-        '/text_channels': message.guild.text_channels,
-        '/voice_channels': message.guild.voice_channels,
-        '/category_channels': message.guild.categories,
-    }
-    return data_table.get(command, 'not command')
 
 
 
-@bot.event
-async def on_message(message):
-    if message.content == "/roles":
-        await message.channel.send(message.guild.roles)
 
 
 bot.run(token)
