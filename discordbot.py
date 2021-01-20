1cb002a7bd81c7b4abe3d3405aab6ef0e2c4a3a9
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-01-20 09:26:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,40 +14,33 @@ async def on_message(message):
     # メッセージの送信者がbotだった場合は無視する
     if message.author.bot:
         return
+    # 狂人on
     elif message.content == "/mad":
         if message.author.voice is None:
-            await message.channel.send("あなたはボイスチャンネルに接続していません")
+            await message.channel.send(f"ボイスチャンネルに接続してからコマンドを入力してください")
             return
         else:
             # コマンド入力者の接続しているボイスチャンネルのmember_listを取得する
-            await message.channel.send(message.author.voice.channel.name)
-            await message.channel.send(message.author.voice.channel.members)
+            await message.channel.send(f"チャンネル名:{message.author.voice.channel.name}")
+            await message.channel.send(f"参加人数:{len(message.author.voice.channel.members)}名")
             # member_listからランダムな1ユーザを選択し、DMを送信する
-            player = await random.choice(message.author.voice.channel.members)
-            dm = await player.create_dm()
-            await dm.send(f"{player}さんは狂人に選ばれました")
-            await message.channel.send("DMを送信しました")
+            dm = await random.choice(message.author.voice.channel.members).create_dm()
+            await dm.send(f"あなたは狂人に選ばれました")
+            await message.channel.send(f"DMを送信しました")
             
-            
+    # てるてるon
     elif message.content == "/teru":
         if message.author.voice is None:
-            await message.channel.send("あなたはボイスチャンネルに接続していません")
+            await message.channel.send(f"ボイスチャンネルに接続してからコマンドを入力してください")
             return
         else:
             # コマンド入力者の接続しているボイスチャンネルのmember_listを取得する
-            await message.channel.send(message.author.voice.channel.name)
-            await message.channel.send(message.author.voice.channel.members)
+            await message.channel.send(f"チャンネル名:{message.author.voice.channel.name}")
+            await message.channel.send(f"参加人数:{len(message.author.voice.channel.members)}名")
             # member_listからランダムな1ユーザを選択し、DMを送信する
-            player = await random.choice(message.author.voice.channel.members)
-            dm = await player.create_dm()
-            await dm.send(f"{player}さんはてるてるに選ばれました")
-            await message.channel.send("DMを送信しました")
-            
-            
-    elif message.content == "/dm":
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention}さんは狂人に選ばれました")
-        await message.channel.send("DMを送信しました")
+            dm = await random.choice(message.author.voice.channel.members).create_dm()
+            await dm.send(f"あなたはてるてるに選ばれました")
+            await message.channel.send(f"DMを送信しました")
 
 @bot.command()
 async def ping(ctx):
