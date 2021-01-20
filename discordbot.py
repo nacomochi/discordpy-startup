3500a550087e6deb9c0a7bb589eb45734ae9508f
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-01-20 09:37:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,37 +14,45 @@ async def on_message(message):
     # メッセージの送信者がbotだった場合は無視する
     if message.author.bot:
         return
+    
     # 狂人on
     elif message.content == "/mad":
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続してからコマンドを入力してください")
+            await message.channel.send(f"***ボイスチャンネルに接続してからコマンドを入力してください***")
             return
         else:
-            # コマンド入力者の接続しているボイスチャンネルのmember_listを取得する
-            await message.channel.send(f"チャンネル名:{message.author.voice.channel.name}")
-            await message.channel.send(f"参加人数:{len(message.author.voice.channel.members)}名")
-            # member_listからランダムな1ユーザを選択し、DMを送信する
-            dm = await random.choice(message.author.voice.channel.members).create_dm()
-            await dm.send(f"あなたは狂人に選ばれました")
-            await message.channel.send(f"DMを送信しました")
+            if 10 >= len(message.author.voice.channel.members) > 0:
+                # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
+                await message.channel.send(f"channel : {message.author.voice.channel.name}")
+                await message.channel.send(f"参加人数 : {len(message.author.voice.channel.members)}名")
+                # member_listからランダムな1ユーザを選択し、DMを送信する
+                dm = await random.choice(message.author.voice.channel.members).create_dm()
+                await dm.send(f"あなたは狂人に選ばれました")
+                await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
+                return
+            else:
+                # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
+                await message.channel.send(f"***ボイスチャンネルのメンバー数が不適切です***")
+                return
             
     # てるてるon
     elif message.content == "/teru":
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続してからコマンドを入力してください")
+            await message.channel.send(f"***ボイスチャンネルに接続してからコマンドを入力してください***")
             return
         else:
-            # コマンド入力者の接続しているボイスチャンネルのmember_listを取得する
-            await message.channel.send(f"チャンネル名:{message.author.voice.channel.name}")
-            await message.channel.send(f"参加人数:{len(message.author.voice.channel.members)}名")
-            # member_listからランダムな1ユーザを選択し、DMを送信する
-            dm = await random.choice(message.author.voice.channel.members).create_dm()
-            await dm.send(f"あなたはてるてるに選ばれました")
-            await message.channel.send(f"DMを送信しました")
+            if 10 >= len(message.author.voice.channel.members) > 0:
+                await message.channel.send(f"channel : {message.author.voice.channel.name}")
+                await message.channel.send(f"参加人数 : {len(message.author.voice.channel.members)}名")
+                # member_listからランダムな1ユーザを選択し、DMを送信する
+                dm = await random.choice(message.author.voice.channel.members).create_dm()
+                await dm.send(f"あなたはてるてるに選ばれました")
+                await message.channel.send(f"てるてるに選ばれた方にDMを送信しました")
+                return
+            else:
+                # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
+                await message.channel.send(f"***ボイスチャンネルのメンバー数が不適切です***")
+                return
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
 
 bot.run(token)
