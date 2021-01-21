eea34f11e663fe77d6b8868680637eee683783ad
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-01-21 21:11:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,6 +26,7 @@ async def on_message(message):
                 await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
                 # member_listからランダムな1ユーザを選択し、DMを送信する
                 dm = await random.choice(message.author.voice.channel.members).create_dm()
+                await message.channel.send(random.choice(message.author.voice.channel.members))
                 await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人はいないことになります\n【勝利条件】インポスター陣営の勝利\n【敗北条件】クルー陣営の勝利")
                 await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
                 return
@@ -44,6 +45,7 @@ async def on_message(message):
                 await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
                 # member_listからランダムな1ユーザを選択し、DMを送信する
                 dm = await random.choice(message.author.voice.channel.members).create_dm()
+                await message.channel.send(random.choice(message.author.voice.channel.members))
                 await dm.send(f"あなたは「**てるてる**」に選ばれました\nあなたがインポスターの場合、てるてるはいないことになります\n【勝利条件】あなたが投票で吊られること\n【敗北条件】インポスターにキルされるor他陣営の勝利")
                 await message.channel.send(f"てるてるに選ばれた方にDMを送信しました")
                 return
