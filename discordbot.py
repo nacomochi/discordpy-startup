753d2c7dedc429250825c7a166a83b8e1e123c12
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-02-05 02:40:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,11 +44,21 @@ async def on_message(message):
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
                 await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
                 # member_listからランダムな1ユーザを選択し、DMを送信する
-                await message.channel.send(random.randint(1,2))
-                dm = await random.sample(message.author.voice.channel.members, random.randint(1,2)).create_dm()
-                await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスター陣営の勝利\n【敗北条件】クルー陣営の勝利")
-                await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
-                return
+                rnd = random.randint(1,2)
+                if rnd == 1:
+                    # member_listからランダムな1ユーザを選択し、DMを送信する
+                    dm = await random.choice(message.author.voice.channel.members).create_dm()
+                    await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスター陣営の勝利\n【敗北条件】クルー陣営の勝利")
+                    await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
+                    return
+                else:
+                    await message.channel.send(random.randint(1,len(message.author.voice.channel.members)))
+                    for i in range(random.randint(1,2)):
+                        len(message.author.voice.channel.members)
+                        dm = await random.sample(message.author.voice.channel.members, random.randint(1,2)).create_dm()
+                        await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスター陣営の勝利\n【敗北条件】クルー陣営の勝利")
+                    await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
+                    return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
                 await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です")
