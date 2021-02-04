b02c4030467f33927b0c702cb980d3387bb39155
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-02-05 07:19:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,11 +2,15 @@ from discord.ext import commands
 import os
 import traceback
 import random
+import asyncio
 
 bot = commands.Bot(
     command_prefix='/'
 )
 token = os.environ['DISCORD_BOT_TOKEN']
+
+activity = discord.Activity(name='my activity', type=discord.ActivityType.watching)
+await client.change_presence(activity=activity)
 
 
 @bot.event
