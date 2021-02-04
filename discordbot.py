c902e97f82b65b4e7b8bd12ba45d308ebc36009b
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-02-05 01:52:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,6 +44,7 @@ async def on_message(message):
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
                 await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
                 # member_listからランダムな1ユーザを選択し、DMを送信する
+                await message.channel.send(random.randint(1,2))
                 dm = await random.sample(message.author.voice.channel.members, random.randint(1,2)).create_dm()
                 await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスター陣営の勝利\n【敗北条件】クルー陣営の勝利")
                 await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
