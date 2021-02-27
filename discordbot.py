4d7a06a45c1b34952c866ed81508075b86c3106d
nacomochi <77700006+nacomochi@users.noreply.github.com>
2021-02-27 19:21:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,37 +1,55 @@
-from discord.ext import commands
 import discord
 import os
 import traceback
 import random
 import asyncio
+from discord.ext import commands
+from discord.ext import tasks
 
 bot = commands.Bot(
     command_prefix='/'
 )
+
 token = os.environ['DISCORD_BOT_TOKEN']
-#token = "ODAxMTk3ODc0NzQ2MDk3Njk2.YAdLqg.vfOc-VP29DS6f9R7YwLPcw_Y4Wg"
+# token = 'ODAxMTk3ODc0NzQ2MDk3Njk2.YAdLqg.EVz3_44IKTeVqPSCNTWMFDybtN4'
+
+    
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+# 指定間隔(60秒に1回)で実行
+@tasks.loop(seconds=60)
+async def change_activity():
+    # botのステータスを変更
+    activity = discord.Activity(name='セキュリティカメラ', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)    
+    
+    
+# tasks.loop の開始直前に実行
+@change_activity.before_loop
+async def before_change_activity():
+    # botがログインするまで(on_readyが発火するまで）待機
+    await client.wait_until_ready()
+
+
+# tasks.loop を開始
+change_activity.start()
+
 
 
 # 起動時に動作する処理
-@bot.event
+@client.event
 async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
+    # 起動したらターミナルにログイン通知を表示
     # print('ログインしました')
 
 
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+# コマンド受信時に動作する処理
 @client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game(name='稼働中'))
-    # activity = discord.Activity(name='Amongus', type=discord.ActivityType.watching)
-    # await client.change_presence(activity=activity)
-    
-
-@bot.event
 async def on_message(message):
-    
-    #説明文作成
+
+    # 説明文作成
     read_me = """【**AmongUs-DMbotコマンド**】
     /mad_1  :  参加者から狂人1名をランダムに選択
     /mad_2  :  参加者から狂人2名をランダムに選択
@@ -45,6 +63,7 @@ async def on_message(message):
     /read_me  :  botの説明文を表示
 
     [備考]
+    **月550時間までしか稼働できないためBOTがオフラインの場合があります**
     ゲーム内に干渉するMODとは別物で、ゲーム自体には一切干渉しません
     コマンド実行者のロールに関わらず使用可能です
     DM送信時にbotから表示された参加人数がボイスチャンネル内の人数と一致しない場合、
@@ -101,75 +120,75 @@ async def on_message(message):
     
 【**Credit**】
     bot制作 : なこもち (TwiiterID : nacomochio)
-    last_update : 2021/02/17"""
-    
-    
+    last_update : 2021/02/27"""
+
+
     # メッセージの送信者がbotだった場合は無視する
     if message.author.bot:
         return
-    
-    
+
+
     # 狂人1名固定on
-    elif message.content == "/mad_1":
+    elif message.content == '/mad_1':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 0:
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # member_listからランダムな1ユーザを選択し、DMを送信する
                 dm = await random.choice(message.author.voice.channel.members).create_dm()
-                await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、無効試合となるので名乗り出てください\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -")
-                await message.channel.send(f"狂人に選ばれた方1名にDMを送信しました")
+                await dm.send(f'あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、無効試合となるので名乗り出てください\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -')
+                await message.channel.send(f'狂人に選ばれた方1名にDMを送信しました')
                 return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください')
                 return
-            
-            
+
+
     # 狂人2名固定on
-    elif message.content == "/mad_2":
+    elif message.content == '/mad_2':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 1:
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # member_listからランダムな2ユーザを選択し、DMを2通送信する
                 mad_list = random.sample(message.author.voice.channel.members, 2)
                 for i in range(2):
                     dm = await mad_list[i-1].create_dm()
-                    await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -")
-                await message.channel.send(f"狂人に選ばれた方2名にDMを送信しました")
+                    await dm.send(f'あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -')
+                await message.channel.send(f'狂人に選ばれた方2名にDMを送信しました')
                 return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n2~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n2~10人の状態でコマンドを実行してください')
                 return
-            
-            
+
+
     # 狂人1or2名on
-    elif message.content == "/mad_randam":
+    elif message.content == '/mad_randam':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 1:
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # 狂人の人数をランダムに選択する
                 rnd = random.randint(1,2)
                 # 狂人1名の場合
                 if rnd == 1:
                     # member_listからランダムな1ユーザを選択し、DMを送信する
                     dm = await random.choice(message.author.voice.channel.members).create_dm()
-                    await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -")
+                    await dm.send(f'あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -')
                     # 処理時間で人数が推測できないように処理を遅延
                     await asyncio.sleep(2)
-                    await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
+                    await message.channel.send(f'狂人に選ばれた方にDMを送信しました')
                     return
                 # 狂人2名の場合
                 else:
@@ -179,87 +198,87 @@ async def on_message(message):
                     # await message.channel.send(mad_list)
                     for i in range(2):
                         dm = await mad_list[i-1].create_dm()
-                        await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -")
+                        await dm.send(f'あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -')
                     # 処理時間で人数が推測できないように処理を遅延
                     await asyncio.sleep(1)
-                    await message.channel.send(f"狂人に選ばれた方にDMを送信しました")
+                    await message.channel.send(f'狂人に選ばれた方にDMを送信しました')
                     return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n2~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n2~10人の状態でコマンドを実行してください')
                 return
-            
-            
+
+
     # 狂人3名固定on
-    elif message.content == "/mad_3":
+    elif message.content == '/mad_3':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 2:
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # member_listからランダムな3ユーザを選択し、DMを2通送信する
                 mad_list = random.sample(message.author.voice.channel.members, 3)
                 for i in range(3):
                     dm = await mad_list[i-1].create_dm()
-                    await dm.send(f"あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -")
-                await message.channel.send(f"狂人に選ばれた方3名にDMを送信しました")
+                    await dm.send(f'あなたは「**狂人**」に選ばれました\nあなたがインポスターの場合、狂人は1人欠けた状態となります\n【勝利条件】インポスターの勝利\n【敗北条件】クルーメイトの勝利\n- - - - -')
+                await message.channel.send(f'狂人に選ばれた方3名にDMを送信しました')
                 return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n3~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n3~10人の状態でコマンドを実行してください')
                 return
-            
-            
+
+
     # てるてるon
-    elif message.content == "/teru_1":
+    elif message.content == '/teru_1':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 0:
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # member_listからランダムな1ユーザを選択し、DMを送信する
                 dm = await random.choice(message.author.voice.channel.members).create_dm()
-                await dm.send(f"あなたは「**てるてる**」に選ばれました\nあなたがインポスターの場合、てるてるは欠けた状態となります\n【勝利条件】あなたが投票で吊られること\n【敗北条件】インポスターにキルされるor他陣営の勝利\n- - - - -")
-                await message.channel.send(f"てるてるに選ばれた方にDMを送信しました")
+                await dm.send(f'あなたは「**てるてる**」に選ばれました\nあなたがインポスターの場合、てるてるは欠けた状態となります\n【勝利条件】あなたが投票で吊られること\n【敗北条件】インポスターにキルされるor他陣営の勝利\n- - - - -')
+                await message.channel.send(f'てるてるに選ばれた方にDMを送信しました')
                 return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください')
                 return
 
-            
+
     # DMテストコマンド
-    elif message.content == "/dm_test":
+    elif message.content == '/dm_test':
         if message.author.voice is None:
-            await message.channel.send(f"ボイスチャンネルに接続した状態でコマンドを入力してください")
+            await message.channel.send(f'ボイスチャンネルに接続した状態でコマンドを入力してください')
             return
         else:
             if 10 >= len(message.author.voice.channel.members) > 0:
                 # コマンド入力者の接続しているボイスチャンネルのメンバーを取得する
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名')
                 # ボイスチャンネル参加者全員にDMを送信する
                 for i in message.author.voice.channel.members:
                     dm = await i.create_dm()
-                    await dm.send(f"**これはDMのテスト送信です**\nあなたが役職に選ばれた場合、このようにbotからDMが届きます\n- - - - -")
-                await message.channel.send(f"ボイスチャンネル参加者全員にDMをテスト送信しました")
+                    await dm.send(f'**これはDMのテスト送信です**\nあなたが役職に選ばれた場合、このようにbotからDMが届きます\n- - - - -')
+                await message.channel.send(f'ボイスチャンネル参加者全員にDMをテスト送信しました')
                 return
             else:
                 # コマンド入力者の接続しているボイスチャンネルのメンバー数が規定値にない
-                await message.channel.send(f"channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください")
+                await message.channel.send(f'channel : {message.author.voice.channel.name}\n参加人数 : {len(message.author.voice.channel.members)}名\nDMを送信できませんでした。ボイスチャンネルの参加人数が不適切です\n1~10人の状態でコマンドを実行してください')
                 return
-            
-            
+
+
     # bot動作確認コマンド
-    elif message.content == "/status":
-        await message.channel.send(f"bot起動中です")
+    elif message.content == '/status':
+        await message.channel.send(f'bot起動中です')
         return
-    
+
 
     # コマンドリスト表示
-    elif message.content == "/command":
+    elif message.content == '/command':
         await message.channel.send(f"""【**AmongUs-DMbotコマンド**】(ロールに関わらず使用可能です)
     /mad_1  :  参加者から狂人1名をランダムに選択
     /mad_2  :  参加者から狂人2名をランダムに選択
@@ -273,73 +292,73 @@ async def on_message(message):
     /read_me  :  botの説明文を表示""")
         return
 
-    
+
     # readme表示
-    elif message.content == "/read_me":
+    elif message.content == '/read_me':
         await message.channel.send(read_me)
         return
-    
-     
+
+
     # 隠しコマンド
-    elif message.content == "よしおちゃん天才って言って":
+    elif message.content == 'よしおちゃん天才って言って':
         rnd = random.randint(1,3)
         if rnd == 1:
-            await message.channel.send(f"ﾖｼｵﾁｬﾝﾃﾝｻｲﾔｯﾀｰ!!!<:aupurple:787851727821996042>")
+            await message.channel.send(f'ﾖｼｵﾁｬﾝﾃﾝｻｲﾔｯﾀｰ!!!<:aupurple:787851727821996042>')
         elif rnd == 2:
             await asyncio.sleep(1)
-            await message.channel.send(f"こ ろ す ぞ <:aupurple:787851727821996042>")
+            await message.channel.send(f'こ ろ す ぞ <:aupurple:787851727821996042>')
         else:
-            await message.channel.send(f"Yoshio is an Impostor.<:aupurpledead:787851743399641091>")
+            await message.channel.send(f'Yoshio is an Impostor.<:aupurpledead:787851743399641091>')
         return
-    
-    
+
+
     # 隠しコマンド
-    elif message.content == "よしおさんに投票します":
+    elif message.content == 'よしおさんに投票します':
         rnd = random.randint(1,3)
         if rnd == 1:
-            await message.channel.send(f"<:aupurple:787851727821996042><じゃあ、なこもちに入れます")
+            await message.channel.send(f'<:aupurple:787851727821996042><じゃあ、なこもちに入れます')
         elif rnd == 2:
-            await message.channel.send(f"Yoshio is an Impostor.<:aupurpledead:787851743399641091>")
+            await message.channel.send(f'Yoshio is an Impostor.<:aupurpledead:787851743399641091>')
         else:
-            await message.channel.send(f"Yoshio is not an Impostor.<:aupurpledead:787851743399641091>")
+            await message.channel.send(f'Yoshio is not an Impostor.<:aupurpledead:787851743399641091>')
         return
-    
-    
+
+
     # 隠しコマンド
-    elif message.content == "がめんさんに投票します":
+    elif message.content == 'がめんさんに投票します':
         rnd = random.randint(1,3)
         if rnd == 1:
-            await message.channel.send(f"Gamen is an old man.:older_man:")
+            await message.channel.send(f'Gamen is an old man.:older_man:')
         elif rnd == 2:
-            await message.channel.send(f"<:auwhite:787851726308376606><Nuts to you!")
+            await message.channel.send(f'<:auwhite:787851726308376606><Nuts to you!')
         else:
-            await message.channel.send(f"<:auwhitedead:787851742577688597>")
+            await message.channel.send(f'<:auwhitedead:787851742577688597>')
         return
-    
-    
+
+
     # 隠しコマンド
-    elif message.content == "ばななさんに投票します":
+    elif message.content == 'ばななさんに投票します':
         rnd = random.randint(1,3)
         if rnd == 1:
-            await message.channel.send(f"<:aupink:787851721182543922><えぇ～わからんーーー:sob:")
+            await message.channel.send(f'<:aupink:787851721182543922><えぇ～わからんーーー:sob:')
         elif rnd == 2:
-            await message.channel.send(f"<:aupink:787851721182543922><ﾋﾟｰ!ﾋﾟﾋﾟﾋﾟｰ!!!:scream:")
+            await message.channel.send(f'<:aupink:787851721182543922><ﾋﾟｰ!ﾋﾟﾋﾟﾋﾟｰ!!!:scream:')
         else:
-            await message.channel.send(f"<:aupinkdead:787851737360629800><ﾋﾟｰ...ﾋﾟｰ...:pensive:")
+            await message.channel.send(f'<:aupinkdead:787851737360629800><ﾋﾟｰ...ﾋﾟｰ...:pensive:')
         return
-    
-        
+
+
     # テキストチャットログ削除コマンド
-    elif message.content == "/chat_delete":
-        if message.channel.name != "amongus-dmbot":
-            await message.channel.send(f"テキストチャンネル名がamongus-dmbotではありません")
+    elif message.content == '/chat_delete':
+        if message.channel.name != 'amongus-dmbot':
+            await message.channel.send(f'テキストチャンネル名がamongus-dmbotではありません')
             return
         else:
-            await message.channel.send(f"このテキストチャンネルのログを全て削除します")
+            await message.channel.send(f'このテキストチャンネルのログを全て削除します')
             await asyncio.sleep(5)
             await message.channel.purge()
             await message.channel.send(read_me)
             return
 
-        
-bot.run(token)
+
+client.run(token)
